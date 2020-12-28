96f0fd906c8fe42b37c5cfa11bf01a64f465f402
Josh <josh@megans-mbp.home>
2020-12-28 15:44:43 +0000

    FIX: Change IP

--- run.py
+++ run.py
@@ -11,5 +11,5 @@ def index():
 
 if __name__ == "__main__":
     app.run(
-        host=os.environ.get("IP", "192.168.1.208"),
+        host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")))

4a14a1d724302491c86bdc4bcbc2d64b273e6b68
P0shJosh <jphodge.96@gmail.com>
2020-12-28 14:35:52 +0000

    FEAT: Begin rendering html with python

---
+++ run.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("base.html")
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "192.168.1.208"),
+        port=int(os.environ.get("PORT", "5000")))
